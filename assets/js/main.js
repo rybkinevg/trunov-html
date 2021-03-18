400a670339aa017877121c96ab0eef2fcb43f91a
rybkinevg <boomuk-mc@yandex.ru>
2021-03-18 12:12:34 +0300

    added slick slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const closeMenuBtn = document.querySelector('.nav__btn--close');
 const subMenuBtn = document.querySelectorAll('.nav__btn--submenu');
 const subMenu = document.querySelectorAll('.submenu');
 const menu = document.querySelector('.nav__inner');
+const body = document.querySelector('.body');
 
 openMenuBtn.addEventListener('click', () => {
 
@@ -18,6 +19,20 @@ subMenuBtn.forEach((btn, index) => {
 
     btn.addEventListener('click', () => {
 
-        subMenu[index].classList.toggle('open');
+        $(subMenu[index]).slideToggle(300);
     })
+});
+
+const isMobile = /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iP(ad|od|hone)/i.test(navigator.userAgent);
+
+if (isMobile) {
+
+    body.classList.add('touch');
+} else {
+
+    body.classList.add('no-touch');
+}
+
+$(document).ready(function () {
+    $('.your-class').slick();
 });
